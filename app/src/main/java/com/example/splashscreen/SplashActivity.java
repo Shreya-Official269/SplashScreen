605c53e18b7d3f50d953dfef7186684c6e38ee20
Kaduburi-Shreya <kaduburishreya@gmail.com>
2024-03-16 16:10:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(intent);
+                finish();
+
+            }
+        },4000);
+
+    }
+}
